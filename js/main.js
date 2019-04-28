b18d7055d3ba57b4d61cdb826ec33f5be84317fb
UlviAlakbarli <46805918+UlviAlakbarli@users.noreply.github.com>
2019-04-28 17:48:51 +0400

    Update v1.2-Fixed bugs and add new features

--- js/main.js
+++ js/main.js
@@ -13,7 +13,9 @@ let submit2=document.querySelector(".submit-2");
 let form2=document.querySelector(".form-2");
 let tbody=document.querySelector("tbody");
 let table=document.querySelector(".table");
+
 let no=1;
+let employeeId=1;
 
 let rotatecounty=0;
 let rotatecount=0;
@@ -29,8 +31,7 @@ window.setInterval( function(){
             
         }},2000
 )
-logo.onclick=function(){
-    
+window.setInterval( function(){
     if(rotatecounty===0){
         logo.style.transform="rotateY(360deg)";
         rotatecounty++
@@ -39,10 +40,8 @@ logo.onclick=function(){
         rotatecounty--;
         logo.style.transform="rotateY(-360deg)";
         
-    }
+    }},4000)
 
-    
-}
 
 
 class Company
@@ -65,6 +64,10 @@ class Company
     {
         return this.allposition.find(item=>item.position==name);
     }
+    filterEmployeeById(id){
+        return this.allemployees=this.allemployees.filter(emp=>emp.id!=id);
+    }
+    
 }
 class Position
 {
@@ -73,22 +76,20 @@ class Position
            this.position=position;
            this.wages=wages;
            no++;
-          
     }
-    
 }
 
 class Employee// extends Position
-{
-
-    constructor(name,surname,age,email,position){
+{ constructor(name,surname,age,email,position){
+            this.id=employeeId;
             this.name=name;
             this.surname=surname;
             this.age=age;
             this.email=email;
             this.position=position;
+            
+            employeeId++;
      }
-
 }
 
 //Creata Company
@@ -96,25 +97,54 @@ let newCompany=new Company;
 
 submit1.onclick=function(ev){
     ev.preventDefault()
+    
       if(inposition.value.trim().length>0&&inwages.value.trim().length>0){
+        if(newCompany.allposition.length>0){
+            
 
-        let newposition= new Position(inposition.value.trim(),inwages.value.trim());
-        newCompany.addPositonToList(newposition)
-        let option=document.createElement("option");
-        option.innerText=inposition.value.trim();
-        option.value=no-1;
-        select.appendChild(option);
-        inposition.value="";
-        inwages.value="";
-        form2.classList.remove("d-none");
-
+            if(ceckPositonName()===false){
+                let newposition= new Position(inposition.value.trim(),inwages.value.trim());
+                 newCompany.addPositonToList(newposition)
+                 let option=document.createElement("option");
+                option.innerText=inposition.value.trim();
+                option.value=no-1;
+                select.appendChild(option);
+                inposition.value="";
+                inwages.value="";
+                 form2.classList.remove("d-none");
+    
+            }
+            else{
+               
+           alert("This position alredy defined")
+            }
+        }
+          else{
+             
+              let newposition= new Position(inposition.value.trim(),inwages.value.trim());
+              newCompany.addPositonToList(newposition)
+              let option=document.createElement("option");
+              option.innerText=inposition.value.trim();
+              option.value=no-1;
+              select.appendChild(option);
+              inposition.value="";
+              inwages.value="";
+              form2.classList.remove("d-none");
+          }
+        
       }
-      
+ }
+  function ceckPositonName(){
+   let ceck=newCompany.findPositionByName(inposition.value.trim());
+    if(ceck!=undefined){
+        return true;
+    }
+    else{
+        return false;
+    }
 
-      
-
-      
 }
+
 function positionFiender(){
     let b=newCompany.findPosition(select.value).position;
     return b;
@@ -147,23 +177,33 @@ submit2.onclick=function(ev){
                     
 
                     td.innerText=item[key];
+                    td.style.textTransform="capitalize";
                     tr.appendChild(td);
 
                 }
                 let tdsalary=document.createElement("td");
+                let tdDismiss=document.createElement("td");
                 let nm=item.position;
                 tdsalary.innerText=positionNameFinder(nm);
+                tdDismiss.classList.add("dismiss");
+                tdDismiss.innerText="Dismiss this employee"
+                
+                tdsalary.style.textTransform="capitalize";
                 tr.appendChild(tdsalary);
-                
-                
+                tr.appendChild(tdDismiss);
                 tbody.appendChild(tr);
+                let butondismiss=document.querySelectorAll(".dismiss");
+                [...butondismiss].forEach(btn=>btn.onclick=function(){
+                    let htmlId=this.parentElement.firstElementChild.innerText;
+                    newCompany.allemployees=newCompany.filterEmployeeById(htmlId);
+                    this.parentElement.innerHTML="";
+
+                });
                 inname.value="";
                 insurname.value="";
                 inage.value="";
                 inemail.value="";
                 select.value="a";
-
-        
                 table.classList.remove("d-none");
 
             }
@@ -172,6 +212,9 @@ submit2.onclick=function(ev){
 
     }
     
-   
-      
 }
+
+
+
+
+
